5ca6aa3dd3f1c5e708c113b4202e11209c8749b2
anyegarcia <50560792+anyegarcia@users.noreply.github.com>
2021-03-17 16:13:32 +0100

    Update Item.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Item(props) {
     console.log(props)
     return (
-        <ul>
+        <ul className="card" style={{"width": "40%", "listStyle": "none"}}>
+            <li><img class="card-img-top" src={props.pictureUrl}/></li>
             <li>id: {props.id}</li>
             <li>title: {props.title}</li>
             <li>description: {props.description}</li>
             <li>price: {props.price}</li>
-            <li><img src={props.pictureUrl}/></li>
         </ul>
     )
 }
