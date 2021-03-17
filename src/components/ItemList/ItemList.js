89126622f291d47ad4cd52928ca210bfe563a889
anyegarcia <50560792+anyegarcia@users.noreply.github.com>
2021-03-17 15:03:29 +0100

    Update ItemList.js

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,11 +2,19 @@ import Item from '../Item/Item'
 import React from 'react'
 
 export default function ItemList(props) {
+    // console.log(props.items)
     return (
-        <ul>
+        <>
             {props.items.map( (x,index) =>(
-                <Item key = {index} item = {x} />
+                <Item
+                key = {index}
+                id = {x.id}
+                title = {x.title}
+                price ={x.description}
+                description ={x.price}
+                pictureUrl = {x.pictureUrl}
+                />
             ))}
-        </ul>
+        </>
     )
 }
