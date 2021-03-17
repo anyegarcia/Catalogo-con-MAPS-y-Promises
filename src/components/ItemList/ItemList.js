053ce70bd808e4a0ca929ebb8f9cd49cd60586a5
anyegarcia <50560792+anyegarcia@users.noreply.github.com>
2021-03-17 16:11:18 +0100

    Update ItemList.js

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,17 +4,17 @@ import React from 'react'
 export default function ItemList(props) {
     // console.log(props.items)
     return (
-        <>
+        <div class="d-flex justify-content-center" style={{"width": "80%;"}}>
             {props.items.map( (x,index) =>(
                 <Item
                 key = {index}
                 id = {x.id}
                 title = {x.title}
-                price ={x.description}
-                description ={x.price}
+                price ={x.price}
+                description ={x.description}
                 pictureUrl = {x.pictureUrl}
                 />
             ))}
-        </>
+        </div>
     )
 }
