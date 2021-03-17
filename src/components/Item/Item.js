81bab103c9cfeecc422ba46aeaed1942c3d16d33
anyegarcia <50560792+anyegarcia@users.noreply.github.com>
2021-03-17 15:02:19 +0100

    Update Item.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export default function Item(props) {
+    console.log(props)
     return (
-        <li>{props.item}</li>
+        <ul>
+            <li>id: {props.id}</li>
+            <li>title: {props.title}</li>
+            <li>description: {props.description}</li>
+            <li>price: {props.price}</li>
+            <li><img src={props.pictureUrl}/></li>
+        </ul>
     )
 }
